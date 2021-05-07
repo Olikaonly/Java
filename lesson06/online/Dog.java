f9f075c50826c31ec48eb97348024dc0ff782ebe
Olga Shestakova <eolgis@gmail.com>
2021-05-07 17:28:19 +0700

    Create Dog.java

---
+++ lesson06/online/Dog.java
@@ -0,0 +1,12 @@
+package lesson06.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 06.05.2021
+ */
+public class Dog extends Animals {
+
+    public Dog(String name, int run, int swim) {
+        super(name, run, swim);
+    }
+}
