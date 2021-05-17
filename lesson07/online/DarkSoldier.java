7f49432dfaf103092a2133c71af359f9d30be4e0
Olga Shestakova <eolgis@gmail.com>
2021-05-17 18:55:48 +0700

    Create DarkSoldier.java

---
+++ lesson07/online/DarkSoldier.java
@@ -0,0 +1,15 @@
+package lesson07.online;
+
+import lesson07.online.models.Soldier;
+
+/**
+ * Created by Olga Shestakova
+ * Date 14.05.2021
+ */
+public class DarkSoldier extends Soldier {
+
+    public DarkSoldier(String name, int attack, float defense, float hp, float heal) {
+
+        super(name, attack, defense, hp, heal);
+    }
+}
