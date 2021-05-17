17cdad86d356ae14d3adc137b33707eb7010d86e
Olga Shestakova <eolgis@gmail.com>
2021-05-17 18:55:56 +0700

    Create LightSoldier.java

---
+++ lesson07/online/LightSoldier.java
@@ -0,0 +1,15 @@
+package lesson07.online;
+
+import lesson07.online.models.Soldier;
+
+/**
+ * Created by Olga Shestakova
+ * Date 14.05.2021
+ */
+public class LightSoldier extends Soldier {
+
+    public LightSoldier(String name, int attack, float defense, float hp, float heal) {
+
+        super(name, attack, defense, hp, heal);
+    }
+}
