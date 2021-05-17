74ec9691d69c32174e7f7d91a721cfff2d5cf1e3
Olga Shestakova <eolgis@gmail.com>
2021-05-17 18:55:52 +0700

    Create HealerSoldier.java

---
+++ lesson07/online/HealerSoldier.java
@@ -0,0 +1,17 @@
+package lesson07.online;
+
+import lesson07.online.models.Soldier;
+
+/**
+ * Created by Olga Shestakova
+ * Date 14.05.2021
+ */
+public class HealerSoldier extends Soldier {
+
+    public HealerSoldier(String name, int attack, float defense, float hp, float heal) {
+
+        super(name, attack, defense, hp, heal);
+    }
+
+}
+
