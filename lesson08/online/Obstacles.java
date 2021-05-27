a033bd22f797ec63175289ffa618f0a9d09d8f8e
Olga Shestakova <eolgis@gmail.com>
2021-05-28 01:03:42 +0700

    Create Obstacles.java

---
+++ lesson08/online/Obstacles.java
@@ -0,0 +1,10 @@
+package lesson08.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 25.05.2021
+ */
+public interface Obstacles {
+    void run(Obstacles obstacles);
+    void jump(Obstacles obstacles);
+}
