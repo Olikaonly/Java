919fab0d30335a7358e061eef5783e47d1018fe6
Olga Shestakova <eolgis@gmail.com>
2021-05-28 01:03:38 +0700

    Create Human.java

---
+++ lesson08/online/Human.java
@@ -0,0 +1,26 @@
+package lesson08.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 25.05.2021
+ */
+public class Human implements OvercomeObstacles {
+
+    private String name;
+
+    public Human(String name) {
+
+        this.name = name;
+    }
+    @Override
+    public void run(Obstacles obstacles) {
+        obstacles.run(obstacles);
+        System.out.println(name + " Бежит по дорожке");
+    }
+    @Override
+    public void jump(Obstacles obstacles) {
+        obstacles.jump(obstacles);
+        System.out.println(name + " Прыгает через стену");
+    }
+
+}
