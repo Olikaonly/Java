8d0855b6fed7718828b8aee6f0784b871f988d7e
Olga Shestakova <eolgis@gmail.com>
2021-05-27 20:48:53 +0700

    Create Cat.java

---
+++ lesson08/online/Cat.java
@@ -0,0 +1,25 @@
+package lesson08.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 25.05.2021
+ */
+public class Cat implements OvercomeObstacles {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+
+    public void run(Obstacles obstacles) {
+        obstacles.run(obstacles);
+        System.out.println(name + " Бежит по дорожке");
+    }
+
+    public void jump(Obstacles obstacles) {
+        obstacles.jump(obstacles);
+        System.out.println(name + " Прыгает через стену");
+    }
+
+}
