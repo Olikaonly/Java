22f96e182bfdc202cda3c3d91a0ac5be5ffd307a
Olga Shestakova <eolgis@gmail.com>
2021-05-28 01:03:53 +0700

    Create Treadmill.java

---
+++ lesson08/online/Treadmill.java
@@ -0,0 +1,15 @@
+package lesson08.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 25.05.2021
+ */
+public class Treadmill implements Obstacles {
+
+    @Override
+    public void run(Obstacles obstacles) {
+    }
+    @Override
+    public void jump(Obstacles obstacles) {
+    }
+}
