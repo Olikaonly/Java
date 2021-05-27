80087e5701adcd694bc4ced8818e338c18414369
Olga Shestakova <eolgis@gmail.com>
2021-05-28 01:03:58 +0700

    Create Wall.java

---
+++ lesson08/online/Wall.java
@@ -0,0 +1,16 @@
+package lesson08.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 25.05.2021
+ */
+public class Wall implements Obstacles {
+
+    @Override
+    public void run(Obstacles obstacles) {
+    }
+    @Override
+    public void jump(Obstacles obstacles) {
+    }
+
+}
