996503f3eb55213fc2d9379a8aeea08f02c6fdfe
Olga Shestakova <eolgis@gmail.com>
2021-06-02 16:07:58 +0700

    Create MyArraySizeException.java

---
+++ lesson09/online/MyArraySizeException.java
@@ -0,0 +1,12 @@
+package lesson09.online;
+
+/**
+ * Created by Olga Shestakova
+ * Date 01.06.2021
+ */
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException () {
+        super();
+    }
+}
