85deea3fbfe2fb8cdddfbb8e41b47343b53e5452
Olga Shestakova <eolgis@gmail.com>
2021-06-17 20:08:03 +0700

    Create ServerApp.java

---
+++ lesson13/ServerApp.java
@@ -0,0 +1,18 @@
+package geek.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Olga Shestakova
+ * Date 16.06.2021
+ */
+public class ServerApp {
+    public static void main(String[] args) {
+
+    new Server();// создали сервер
+    }
+}
